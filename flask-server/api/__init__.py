d179bd3e988568848d3ddba2df0175ecdc564304
Aiman <aimanaaw@gmail.com>
2020-03-27 17:39:30 -0400

    Refactoring and organizing modules

--- flask-server/api/__init__.py
+++ flask-server/api/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-# app = Flask(__name__)
+app = Flask(__name__)
 
 # @app.route("/")
 # def homepage():
@@ -8,8 +8,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 # if __name__ == '__main__':
 #   app.run(debug=True, host='0.0.0.0')
-
-db = SQLAlchemy()
 postgres = {
   'user': 'aimanaaw',
   'pw': 'password',
@@ -17,15 +15,32 @@ postgres = {
   'host': 'localhost',
   'port': '5432'
 }
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://aimanaaw:password@localhost/my_database'
+db = SQLAlchemy(app)
+db.init_app(app)
+from .routes import main
+app.register_blueprint(main)
 
-def create_app():
-  app = Flask(__name__)
+from api import routes
 
-  app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://aimanaaw:password@localhost/my_database'
 
-  db.init_app(app)
 
-  from .views import main
-  app.register_blueprint(main)
+# postgres = {
+#   'user': 'aimanaaw',
+#   'pw': 'password',
+#   'db': 'my_database',
+#   'host': 'localhost',
+#   'port': '5432'
+# }
 
-  return app
+# def create_app():
+#   app = Flask(__name__)
+
+#   app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://aimanaaw:password@localhost/my_database'
+
+#   db.init_app(app)
+
+#   from .views import main
+#   app.register_blueprint(main)
+
+#   return app
