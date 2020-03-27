0c22519cc2ae94e79f8d29c621785f573a71923c
Aiman <aimanaaw@gmail.com>
2020-03-27 02:50:55 -0400

    Working Post route

--- flask-server/api/views.py
+++ flask-server/api/views.py
@@ -1,9 +1,15 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from . import db
+from .models import Comments
 
 main = Blueprint('main', __name__)
 
 @main.route('/add_comments', methods=['POST'])
 def add_comments():
+  comment_data = request.get_json()
+  new_comment = Comments(author=comment_data['author'], note=comment_data['note'], email=comment_data['email'], date=comment_data['date'])
+  db.session.add(new_comment)
+  db.session.commit()
   
   return 'Done', 201
 
