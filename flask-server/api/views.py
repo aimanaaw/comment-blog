1c684b7bdefb76d381e1922a710a8b79e7f96555
Aiman <aimanaaw@gmail.com>
2020-03-27 02:56:55 -0400

    Both endpoints working

--- flask-server/api/views.py
+++ flask-server/api/views.py
@@ -15,7 +15,10 @@ def add_comments():
 
 @main.route('/comments')
 def comments():
+  comment_list = Comments.query.all()
 
   comments = []
+  for comment in comment_list:
+    comments.append({'author': comment.author, 'note': comment.note, 'email': comment.email, 'date': comment.date})
 
   return jsonify({'comments': comments})
