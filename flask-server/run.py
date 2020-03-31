d05fabb7bff33ed9ddf6c77eb85c7b9a00ae31da
Aiman <aimanaaw@gmail.com>
2020-03-31 15:17:45 -0400

    Final test before dockerizing

--- flask-server/run.py
+++ flask-server/run.py
@@ -1,7 +1,7 @@
 from api import app
 from flask_socketio import SocketIO, send
 
-socketio = SocketIO(app, debug=True, cors_allowed_origins="*")
+socketio = SocketIO(app, debug=True, cors_allowed_origins="*", host = '0.0.0.0')
 
 @socketio.on("message")
 def handleMessage(msg):
